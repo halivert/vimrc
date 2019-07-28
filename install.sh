0f142f1e3c428d6d51eb5df2cd805f498c29f75b
Hali V <halivelazquez@outlook.com>
2019-07-28 00:20:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ scriptPath=`realpath $0`
 configPath=`dirname $scriptPath`
 
 OPTS=$(getopt -o pfas \
-	--long output:plugins,functions,all,secret \
+	--long output:,plugins,functions,all,secret \
 	-n 'parse-options' -- "$@")
 
 if [ $? != 0 ]; then
@@ -30,30 +30,39 @@ while true; do
 	esac
 done
 
-echo "let configPath = \"$configPath\"" > $OUTPUT_FILE
-echo "so $configPath/.vimrc" >> $OUTPUT_FILE
-echo "" >> $OUTPUT_FILE
+if [ -e $OUTPUT_FILE ] ; then
+	echo "Renaming $OUTPUT_FILE to $OUTPUT_FILE.bk"
+	mv $OUTPUT_FILE $OUTPUT_FILE.bk
+fi
 
-if [ "$PLUGINS" = true ] || [ "$ALL" = true ] ; then
-	echo "Adding plugins.vim"
-	if ! [ -e ~/.vim/autoload/plug.vim ] ; then
-		echo "Installing vim plug..."
-		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -n $OUTPUT_FILE ] ; then
+	if ! [ -e $OUTPUT_FILE ] || [ "$FORCE" = true ] ; then
+		echo "let configPath = \"$configPath\"" > $OUTPUT_FILE
+		echo "so $configPath/.vimrc" >> $OUTPUT_FILE
+		echo "" >> $OUTPUT_FILE
 
+		if [ "$PLUGINS" = true ] || [ "$ALL" = true ] ; then
+			echo "Adding plugins.vim"
+			if ! [ -e ~/.vim/autoload/plug.vim ] ; then
+				echo "Installing vim plug..."
+				curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+					https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+			fi
+
+			echo "so $configPath/plugins.vim" >> $OUTPUT_FILE
+		fi
+
+		if [ "$FUNCTIONS" = true ] || [ "$ALL" = true ] ; then
+			echo "Adding functions.vim"
+			echo "so $configPath/functions.vim" >> $OUTPUT_FILE
+		fi
+
+		if [ "$SECRET" = true ] || [ "$ALL" = true ] ; then
+			echo "Adding secret.vim"
+			echo "so $HOME/.vim/secret.vim" >> $OUTPUT_FILE
+		fi
+
+		echo "$OUTPUT_FILE written"
 	fi
-
-	echo "so $configPath/plugins.vim" >> $OUTPUT_FILE
 fi
-
-if [ "$FUNCTIONS" = true ] || [ "$ALL" = true ] ; then
-	echo "Adding functions.vim"
-	echo "so $configPath/functions.vim" >> $OUTPUT_FILE
-fi
-
-if [ "$SECRET" = true ] || [ "$ALL" = true ] ; then
-	echo "Adding secret.vim"
-	echo "so $HOME/.vim/secret.vim" >> $OUTPUT_FILE
-fi
-
-echo "$OUTPUT_FILE written"
