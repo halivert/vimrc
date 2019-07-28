6c5668ac00ae2f36a0ad24aa1ab87a9c75ef8288
Hali V <halivelazquez@outlook.com>
2019-07-28 13:02:26 -0500

    Add minimal to install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 scriptPath=`realpath $0`
 configPath=`dirname $scriptPath`
 
-OPTS=$(getopt -o pfas \
-	--long output:,plugins,functions,all,secret \
+OPTS=$(getopt -o pfasm \
+	--long output:,plugins,minimal,functions,all,secret \
 	-n 'parse-options' -- "$@")
 
 if [ $? != 0 ]; then
@@ -17,6 +17,7 @@ PLUGINS=false
 FUNCTIONS=false
 SECRET=false
 ALL=false
+MINIMAL=false
 
 while true; do
 	case "$1" in
@@ -25,44 +26,54 @@ while true; do
 		-f | --functions ) FUNCTIONS=true; shift ;;
 		-s | --secret ) SECRET=true; shift ;;
 		-a | --all ) ALL=true; shift ;;
+		-m | --minimal ) MINIMAL=true; shift ;;
 		-- ) shift; break ;;
 		* ) break ;;
 	esac
 done
 
-if [ -e $OUTPUT_FILE ] ; then
-	echo "Renaming $OUTPUT_FILE to $OUTPUT_FILE.bk"
-	mv $OUTPUT_FILE $OUTPUT_FILE.bk
-fi
+if [ "$MINIMAL" = false ] ; then
+	if [ -e $OUTPUT_FILE ] ; then
+		echo "Renaming $OUTPUT_FILE to $OUTPUT_FILE.bk"
+		mv $OUTPUT_FILE $OUTPUT_FILE.bk
+	fi
 
-if [ -n $OUTPUT_FILE ] ; then
-	if ! [ -e $OUTPUT_FILE ] || [ "$FORCE" = true ] ; then
-		echo "let configPath = \"$configPath\"" > $OUTPUT_FILE
-		echo "so $configPath/.vimrc" >> $OUTPUT_FILE
-		echo "" >> $OUTPUT_FILE
+	if [ -n $OUTPUT_FILE ] ; then
+		if ! [ -e $OUTPUT_FILE ] || [ "$FORCE" = true ] ; then
+			echo "let configPath = \"$configPath\"" > $OUTPUT_FILE
+			echo "so $configPath/.vimrc" >> $OUTPUT_FILE
+			echo "" >> $OUTPUT_FILE
 
-		if [ "$PLUGINS" = true ] || [ "$ALL" = true ] ; then
-			echo "Adding plugins.vim"
-			if ! [ -e ~/.vim/autoload/plug.vim ] ; then
-				echo "Installing vim plug..."
-				curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-					https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+			if [ "$PLUGINS" = true ] || [ "$ALL" = true ] ; then
+				echo "Adding plugins.vim"
+				if ! [ -e ~/.vim/autoload/plug.vim ] ; then
+					echo "Installing vim plug..."
+					curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+						https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+				fi
+
+				echo "so $configPath/plugins.vim" >> $OUTPUT_FILE
 			fi
 
-			echo "so $configPath/plugins.vim" >> $OUTPUT_FILE
-		fi
+			if [ "$FUNCTIONS" = true ] || [ "$ALL" = true ] ; then
+				echo "Adding functions.vim"
+				echo "so $configPath/functions.vim" >> $OUTPUT_FILE
+			fi
 
-		if [ "$FUNCTIONS" = true ] || [ "$ALL" = true ] ; then
-			echo "Adding functions.vim"
-			echo "so $configPath/functions.vim" >> $OUTPUT_FILE
-		fi
+			if [ "$SECRET" = true ] || [ "$ALL" = true ] ; then
+				echo "Adding secret.vim"
+				echo "so $HOME/.vim/secret.vim" >> $OUTPUT_FILE
+			fi
 
-		if [ "$SECRET" = true ] || [ "$ALL" = true ] ; then
-			echo "Adding secret.vim"
-			echo "so $HOME/.vim/secret.vim" >> $OUTPUT_FILE
+			echo "$OUTPUT_FILE written"
 		fi
-
-		echo "$OUTPUT_FILE written"
 	fi
+else
+	if [ -e $OUTPUT_FILE ] ; then
+		echo "Renaming $OUTPUT_FILE to $OUTPUT_FILE.bk"
+		mv $OUTPUT_FILE $OUTPUT_FILE.bk
+	fi
+
+	echo "so $configPath/.minvimrc" >> $OUTPUT_FILE
 fi
